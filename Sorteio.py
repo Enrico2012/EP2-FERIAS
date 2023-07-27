c4c7f25881eca0565e80da59d8b106a50c3bef61
JulioSza <117858766+JulioSza@users.noreply.github.com>
2023-07-27 00:00:18 -0300

    Update Sorteio.py

--- Sorteio.py
+++ Sorteio.py
@@ -1,13 +1,21 @@
-def sorteia_questao_inedita(questoes, nivel, questoes_sorteadas):
-    questoes_nivel = [questao for questao in questoes if questao['nivel'] == nivel and questao not in questoes_sorteadas]
+import random
 
+def sorteia_questao(dicionario_questoes, nivel):
+    questoes_nivel = dicionario_questoes.get(nivel, [])
     if questoes_nivel:
-        questao_sorteada = random.choice(questoes_nivel)
-        questoes_sorteadas.append(questao_sorteada)
-        return questao_sorteada
+        return random.choice(questoes_nivel)
     else:
-        print(f"Não há mais perguntas inéditas disponíveis para o nível {nivel}.")
         return None
+def sorteia_questao_inedita(questoes, nivel, questoes_sorteadas):
+    questoes_nivel = questoes[nivel]
+    random.shuffle(questoes_nivel)
+    
+    for questao in questoes_nivel:
+        if questao not in questoes_sorteadas:
+            questoes_sorteadas.append(questao)
+            return questao
+
+    return None
 
 def questao_para_texto(questao, id):
     texto = f"----------------------------------------\nQUESTAO {id}\n\n{questao['titulo']}\n\nRESPOSTAS:\n"
